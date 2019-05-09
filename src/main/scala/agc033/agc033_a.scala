7a5992d463e3712e2e483cc30a3bf578c5d69639
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-05-09 21:14:58 +1000

    feat: to be fast with breadth first search

--- src/main/scala/agc033/agc033_a.scala
+++ src/main/scala/agc033/agc033_a.scala
@@ -1,5 +1,7 @@
 package agc033
 
+import scala.collection.mutable
+
 object agc033_a extends App {
   val (h, w) = scala.io.StdIn.readLine.split(' ').map(_.toInt) match { case Array(x, y) => (x, y) }
 
@@ -9,39 +11,34 @@ object agc033_a extends App {
     scala.io.StdIn.readLine.zipWithIndex.foreach { case (value, index) => board(row)(index) = value.toString }
   }
 
+  // 幅優先探索を行う
+
+  val queue   = mutable.Queue[(Int, Int)]()
+  val visited = mutable.Set[(Int, Int)]()
+
+  // 最初に与えられている黒マスをキューに追加する
+  board.zipWithIndex.foreach(row => row._1.zipWithIndex.filter(_._1 == "#").foreach(v => queue += ((row._2, v._2))))
+
+  // 黒マスは訪問済みの印を付ける
+  queue.foreach(v => visited += ((v._1, v._2)))
+
   // 塗りつぶすまでの回数を数える
   var times = 0
-  while (!isFilled(board)) {
-    var target: Seq[(Int, Int)] = Seq()
-    for {
-      i <- board.indices
-      j <- board(0).indices
-    } judge(board, i, j) match {
-      case Some(x) =>
-        target = target ++ x
-      case None =>
+  while (queue.nonEmpty) {
+    // 深さを数えるために 1 つの深さごとにキューを分けて数えられるようにする
+    val nextQueue = mutable.Queue[(Int, Int)]()
+    while (queue.nonEmpty) {
+      val v = queue.dequeue()
+      val nextTo = Seq((v._1 - 1, v._2), (v._1, v._2 + 1), (v._1 + 1, v._2), (v._1, v._2 - 1))
+        .filter(v => v._1 >= 0 && v._1 < h && v._2 >= 0 && v._2 < w)
+        .filterNot(visited.contains)
+      nextQueue ++= nextTo
+      nextTo.foreach(v => visited += ((v._1, v._2)))
     }
+    queue ++= nextQueue
 
-    board = update(board, target, h, w)
-    times = times + 1
+    // キューが空になっていた場合は塗りつぶし終わったということなので回数には加えない
+    if (queue.nonEmpty) times = times + 1
   }
   println(times.toString)
-
-  def isFilled(board: Array[Array[String]]): Boolean = !board.flatten.contains(".")
-
-  def judge(array: Array[Array[String]], i: Int, j: Int): Option[Seq[(Int, Int)]] =
-    if (board(i)(j) == "#") {
-      Some(Seq((i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)))
-    } else {
-      None
-    }
-
-  def update(array: Array[Array[String]], target: Seq[(Int, Int)], h: Int, w: Int): Array[Array[String]] = {
-    target.foreach {
-      case (i, j) =>
-        if (i >= 0 && i < h && j >= 0 && j < w)
-          board(i)(j) = "#"
-    }
-    board
-  }
 }
