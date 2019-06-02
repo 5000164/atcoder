181747fa3b2ee9bb89f082424bad5499a04ea195
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-06-02 22:31:37 +1000

    feat: to be faster

--- src/main/scala/agc033/agc033_a.scala
+++ src/main/scala/agc033/agc033_a.scala
@@ -10,31 +10,22 @@ object agc033_a extends App {
   // 幅優先探索を行う
 
   val queue   = mutable.Queue[(Int, Int)]()
-  val visited = mutable.Set[(Int, Int)]()
+  val visited = mutable.Map.empty[(Int, Int), Int]
 
   // 最初に与えられている黒マスをキューに追加する
   t.zipWithIndex.foreach(r => r._1.zipWithIndex.filter(_._1 == '#').foreach(v => queue += ((r._2, v._2))))
 
   // 黒マスは訪問済みの印を付ける
-  queue.foreach(v => visited += ((v._1, v._2)))
+  queue.foreach(v => visited((v._1, v._2)) = 0)
 
   // 塗りつぶすまでの回数を数える
-  var times = 0
   while (queue.nonEmpty) {
-    // 深さを数えるために 1 つの深さごとにキューを分けて数えられるようにする
-    val nextQueue = mutable.Queue[(Int, Int)]()
-    while (queue.nonEmpty) {
-      val v = queue.dequeue()
-      val nextTo = Seq((v._1 - 1, v._2), (v._1, v._2 + 1), (v._1 + 1, v._2), (v._1, v._2 - 1))
-        .filter(v => v._1 >= 0 && v._1 < h && v._2 >= 0 && v._2 < w)
-        .filterNot(visited.contains)
-      nextQueue ++= nextTo
-      nextTo.foreach(v => visited += ((v._1, v._2)))
-    }
-    queue ++= nextQueue
-
-    // キューが空になっていた場合は塗りつぶし終わったということなので回数には加えない
-    if (queue.nonEmpty) times = times + 1
+    val v = queue.dequeue()
+    val nextTo = Seq((v._1 - 1, v._2), (v._1, v._2 + 1), (v._1 + 1, v._2), (v._1, v._2 - 1))
+      .filter(v => v._1 >= 0 && v._1 < h && v._2 >= 0 && v._2 < w)
+      .filterNot(visited.contains)
+    queue ++= nextTo
+    nextTo.foreach(nv => visited((nv._1, nv._2)) = visited((v._1, v._2)) + 1)
   }
-  println(times.toString)
+  println(visited.maxBy(_._2)._2)
 }
