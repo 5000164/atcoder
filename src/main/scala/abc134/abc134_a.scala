05c4cc3698e1d090e2e988222aa73f58fbf34c00
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-07-20 22:52:54 +1000

    feat: to answer abc134_a

---
+++ src/main/scala/abc134/abc134_a.scala
@@ -0,0 +1,10 @@
+package abc134
+
+object abc134_a extends App {
+  val in = new java.util.Scanner(System.in)
+  val r  = in.nextInt
+
+  val answer = 3 * Math.pow(r, 2).toInt
+
+  println(answer)
+}
